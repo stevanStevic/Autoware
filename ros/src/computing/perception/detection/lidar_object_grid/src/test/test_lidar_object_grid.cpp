ff4fbf8e2e936d0984d162a417a26e198cb046e9
Marko Dragojevic <marko.godra@gmail.com>
2019-03-26 20:27:06 +0100

    Implemented ros publishtest for lidar_object_grid (Integration test) 	Moved tf related test to different integration test lidar_object_grid-tf-test 	rostest needs to be used for this integration test to pass due the inability to pass 	additional arguments to add_rostest_gtest() macro (--reuse-master flag needs to be passed)

--- ros/src/computing/perception/detection/lidar_object_grid/src/test/test_lidar_object_grid.cpp
+++ ros/src/computing/perception/detection/lidar_object_grid/src/test/test_lidar_object_grid.cpp
@@ -280,30 +280,6 @@ TEST_F(FilterTest, correctlyFiltered)
   EXPECT_EQ(3, filteredCloud.size());
 }
 
-TEST_F(LidarObjectGridTest, aquireTransformationRetVal)
-{
-  tf::StampedTransform transformation;
-
-  bool rv = getTransforamtion(transformation);
-
-  EXPECT_TRUE(rv);
-}
-
-TEST_F(LidarObjectGridTest, aquireTransformationOutput)
-{
-  tf::StampedTransform correctTransformation;
-  tf::StampedTransform transformation;
-
-  // Values acquired from rostopic echo /tf
-  correctTransformation.setOrigin({1.2,0.0,2.0});
-  correctTransformation.setRotation({0.0, 0.0, 0.0,1.0});
-
-  ASSERT_TRUE(getTransforamtion(transformation));
-
-  EXPECT_EQ(correctTransformation.getOrigin(), transformation.getOrigin());
-  EXPECT_EQ(correctTransformation.getRotation(), transformation.getRotation());
-}
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "lidar_object_grid_test");
---
+++ ros/src/computing/perception/detection/lidar_object_grid/test/test_lidar_object_grid_tf.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+
+#include "lidar_object_grid/lidar_object_grid.hpp"
+
+class LidarObjectGridTest : public testing::Test, public LidarObjectGrid
+{
+protected:
+private:
+  ros::NodeHandle nodeHandle; //!< ROS node handle
+};
+
+TEST_F(LidarObjectGridTest, aquireTransformationRetVal)
+{
+  tf::StampedTransform transformation;
+
+  bool rv = getTransforamtion(transformation);
+
+  EXPECT_TRUE(rv);
+}
+
+TEST_F(LidarObjectGridTest, aquireTransformationOutput)
+{
+  tf::StampedTransform correctTransformation;
+  tf::StampedTransform transformation;
+
+  // Values acquired from rostopic echo /tf
+  correctTransformation.setOrigin({1.2,0.0,2.0});
+  correctTransformation.setRotation({0.0, 0.0, 0.0,1.0});
+
+  ASSERT_TRUE(getTransforamtion(transformation));
+
+  EXPECT_EQ(correctTransformation.getOrigin(), transformation.getOrigin());
+  EXPECT_EQ(correctTransformation.getRotation(), transformation.getRotation());
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "lidar_object_grid_tf_test");
+
+    ros::shutdown();
+    return RUN_ALL_TESTS();
+}
